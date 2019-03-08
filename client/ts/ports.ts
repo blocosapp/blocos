5b4dd780917b8301b7f8414c710cf6085c69efc7
Vinicius Andrade <vini.andrade.dev@gmail.com>
2019-03-08 19:26:49 +0100

    Split typescript module and move'em to `ts` folder
    Signed-off-by: Vinicius Andrade <vini.andrade.dev@gmail.com>

---
+++ client/ts/ports.ts
@@ -0,0 +1,49 @@
+import * as blockstack from 'blockstack'
+import { App } from './Application'
+
+type Project = {
+  id?: string,
+  address?: string,
+  description: string,
+  goal: number,
+  title: string
+}
+
+export function authenticate (): void {
+  blockstack
+    .redirectToSignIn(
+      'https://in.blocos.app',
+      'https://blocos.app/manifest.json',
+      ['scope']
+    )
+}
+
+export function handleAuthentication (app: App): void {
+  if (blockstack.isUserSignedIn()) {
+    const user = blockstack.loadUserData()
+    if (user) {
+      return app.ports.authenticated.send(user)
+    }
+
+  }
+  if (blockstack.isSignInPending()) {
+    blockstack
+      .handlePendingSignIn()
+      .then(user => {
+        if (user) {
+          return app.ports.authenticated.send(user)
+        }
+      })
+  }
+}
+
+export function putFile (project: Project): void {
+  const fileName = project.id
+  const fileContent = JSON.stringify(project)
+  blockstack
+    .putFile(fileName + '.json', fileContent)
+}
+
+export function signOut (): void {
+  blockstack.signUserOut('/')
+}
