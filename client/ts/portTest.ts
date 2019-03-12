7c4eb8cfa3898ebb49775e458fbf08b78a8c95f4
Vinicius Andrade <vini.andrade.dev@gmail.com>
2019-03-12 08:23:58 +0100

    Replace Jest + Babel with Jest + ts-jest
    As there are some unwanted limitations in using jest with babel.
    
    Signed-off-by: Vinicius Andrade <vini.andrade.dev@gmail.com>

--- client/ts/portTest.ts
+++ client/ts/portTest.ts
@@ -1,4 +1,4 @@
-import * as blockstackMock from 'blockstack'
+import * as blockstack from 'blockstack'
 import { authenticate, handleAuthentication, putFile, signOut } from './ports'
 import { flushPromises } from '../../helpers/specHelpers'
 
@@ -10,48 +10,58 @@ jest.mock('blockstack', () => ({
   redirectToSignIn: jest.fn()
 }))
 
-function mockAuthenticatedUser (user) {
+function mockAuthenticatedUser (blockstackMock, user) {
   blockstackMock.isUserSignedIn.mockImplementationOnce(() => true)
   blockstackMock.loadUserData.mockImplementationOnce(() => user)
 }
 
-function mockPendingAuthenticationUser (user) {
+function mockPendingAuthenticationUser (blockstackMock, user) {
   blockstackMock.isUserSignedIn.mockImplementationOnce(() => false)
   blockstackMock.isSignInPending.mockImplementationOnce(() => true)
   blockstackMock.handlePendingSignIn.mockImplementationOnce(async () => user)
+}
+
+function generateAppMock () {
+  return {
+    ports: {
+      authenticate: {
+        subscribe: jest.fn()
+      },
+      signOut: {
+        subscribe: jest.fn()
+      },
+      putFile: {
+        subscribe: jest.fn()
+      },
+      checkAuthentication: {
+        subscribe: jest.fn()
+      },
+      authenticated: {
+        send: jest.fn()
+      }
+    }
+  }
 }
 
 describe('port module', () => {
   describe('should handle authentication communication between ports', () => {
     it('should redirect to blockstack sign in on authenticate', () => {
       authenticate()
-      expect(blockstackMock.redirectToSignIn).toHaveBeenCalled()
+      expect(blockstack.redirectToSignIn).toHaveBeenCalled()
     })
 
     it('should handle signed in user and send it through app port "authenticated"', () => {
       const userMock = { username: 'Mr. Mock' }
-      const appMock = {
-        ports: {
-          authenticated: {
-            send: jest.fn()
-          }
-        }
-      }
-      mockAuthenticatedUser(userMock)
+      const appMock = generateAppMock()
+      mockAuthenticatedUser(blockstack, userMock)
       handleAuthentication(appMock)
       expect(appMock.ports.authenticated.send).toHaveBeenCalledWith(userMock)
     })
 
     it('should handle pending sign in and then send user through app port "authenticated"', async () => {
       const userMock = { username: 'Mr. Mock' }
-      const appMock = {
-        ports: {
-          authenticated: {
-            send: jest.fn()
-          }
-        }
-      }
-      mockPendingAuthenticationUser(userMock)
+      const appMock = generateAppMock()
+      mockPendingAuthenticationUser(blockstack, userMock)
       handleAuthentication(appMock)
       await flushPromises()
       expect(appMock.ports.authenticated.send).toHaveBeenCalledWith(userMock)
--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,7 @@
-// For a detailed explanation regarding each configuration property, visit:
-// https://jestjs.io/docs/en/configuration.html
-
 module.exports = {
   clearMocks: true,
   coverageDirectory: "coverage",
-  setupFiles: ['./jest.boot.js'],
-  moduleFileExtensions: ['elm', 'js', 'ts']
+  moduleFileExtensions: ['elm', 'js', 'ts'],
+  preset: 'ts-jest',
+  setupFiles: ['./jest.boot.js']
 }
